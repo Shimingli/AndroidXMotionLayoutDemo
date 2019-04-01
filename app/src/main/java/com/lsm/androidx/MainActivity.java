6763f0a4f74bd90e5fc8b66c57bac5104b1939d2
shiming.li <lamshiming@sina.com>
2019-04-01 11:43:16 +0800

    Initial commit

---
+++ app/src/main/java/com/lsm/androidx/MainActivity.java
@@ -0,0 +1,16 @@
+package com.lsm.androidx;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
